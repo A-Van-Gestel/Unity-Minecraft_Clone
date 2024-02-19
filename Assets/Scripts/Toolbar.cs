09605eb58d5027bfadecc7c0bbae71b4c8dc420b
Axel Van Gestel <axel.van.gestel@gmail.com>
2024-02-19 15:52:22 +0100

    Tutorial 11 - Toolbar access item by number key

--- Assets/Scripts/Toolbar.cs
+++ Assets/Scripts/Toolbar.cs
@@ -14,6 +14,19 @@ public class Toolbar : MonoBehaviour
 
     private int slotIndex = 0;
 
+    private KeyCode[] keyCodes =
+    {
+        KeyCode.Alpha1,
+        KeyCode.Alpha2,
+        KeyCode.Alpha3,
+        KeyCode.Alpha4,
+        KeyCode.Alpha5,
+        KeyCode.Alpha6,
+        KeyCode.Alpha7,
+        KeyCode.Alpha8,
+        KeyCode.Alpha9,
+    };
+
     private void Start()
     {
         world = GameObject.Find("World").GetComponent<World>();
@@ -23,12 +36,13 @@ public class Toolbar : MonoBehaviour
             slot.icon.sprite = world.blockTypes[slot.itemID].icon;
             slot.icon.enabled = true;
         }
-        
+
         player.selectedBlockIndex = itemSlots[slotIndex].itemID;
     }
 
     private void Update()
     {
+        // SCROLL WHEEL
         float scroll = Input.GetAxis("Mouse ScrollWheel");
 
         if (scroll != 0)
@@ -44,9 +58,25 @@ public class Toolbar : MonoBehaviour
             if (slotIndex < 0)
                 slotIndex = itemSlots.Length - 1;
 
-            highlight.position = itemSlots[slotIndex].icon.transform.position;
-            player.selectedBlockIndex = itemSlots[slotIndex].itemID;
+            SetItemSlot();
         }
+
+        
+        // NUMBER KEYS
+        for (int i = 0; i < keyCodes.Length; i++)
+        {
+            if (Input.GetKeyDown(keyCodes[i]))
+            {
+                slotIndex = i;
+                SetItemSlot();
+            }
+        }
+    }
+
+    private void SetItemSlot()
+    {
+        highlight.position = itemSlots[slotIndex].icon.transform.position;
+        player.selectedBlockIndex = itemSlots[slotIndex].itemID;
     }
 }
 
